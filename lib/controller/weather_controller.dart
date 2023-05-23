e9cbd88547d1ae9d59615eb1c7ca1633714871ea
samuel senerwa <samuelsenerwagmil.com>
2023-05-23 15:53:59 +0300

    created controller

--- lib/controller/weather_controller.dart
+++ lib/controller/weather_controller.dart
@@ -1,5 +1,22 @@
 import 'package:get/get.dart';
 import 'package:flutter_application_weather/model/weather_model.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter_application_weather/service/service.dart';
 import 'package:flutter_application_weather/utilities/snack_bar.dart';
 
+class WeatherController extends GetxController {
+  final weatherService = Get.put(WeatherService());
+
+  Future<Weather> getWeatherData() async {
+    var res;
+
+    try {
+      res = await weatherService.getWeather();
+      if (res.statusCode != 200 || res.statusCode != 201) {
+        return WeatherSnackBars.errorSnackBar(message: res.data['message']);
+      } else {}
+    } catch (e) {
+      WeatherSnackBars.errorSnackBar(message: e.toString());
+    }
+    retrun Weather.fromJson(res.data);
+  }
+}
